6db3e8049cbc988d5d66906af697dcc6a3c1f680
GideonJulian <gideonchinonso77@gmail.com>
2026-07-01 14:28:17 +0100

    updated

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,22 +1,24 @@
 import { supabase } from "../lib/supabase";
 
 export const AuthService = {
- async getCurrentUserRole() {
-  const { data: { user } } = await supabase.auth.getUser();
+  async getCurrentUserRole() {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
 
-  if (!user) return null;
+    if (!user) return null;
 
-  const { data, error } = await supabase
-    .from("profile")
-    .select("role")
-    .eq("id", user.id)
-    .maybeSingle();
+    const { data, error } = await supabase
+      .from("profile")
+      .select("role")
+      .eq("id", user.id)
+      .maybeSingle();
 
-  if (error || !data) {
-    console.log("ROLE FETCH ERROR:", error);
-    return null;
-  }
+    if (error || !data) {
+      console.log("ROLE FETCH ERROR:", error);
+      return null;
+    }
 
-  return data.role?.trim().toLowerCase();
-}
+    return data.role?.trim().toLowerCase();
+  },
 };
